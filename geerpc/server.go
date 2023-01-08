b99408b581264b3926857b0b45f08baba35ec7f0
gueFDF <3237455241@qq.com>
2023-01-08 17:06:24 +0800

    服务器支持HTTP协议

--- geerpc/server.go
+++ geerpc/server.go
@@ -1,3 +1,7 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package geerpc
 
 import (
@@ -8,125 +12,89 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
 	"reflect"
 	"strings"
 	"sync"
 	"time"
 )
 
-const MagicNumber = 0x3bef5c //3927900
+const MagicNumber = 0x3bef5c
 
 type Option struct {
-	MagicNumber    int
-	CodcType       codec.Type //客户端可以选择
-	ConnectTimeout time.Duration
+	MagicNumber    int           // MagicNumber marks this's a geerpc request
+	CodcType       codec.Type    // client may choose different Codec to encode body
+	ConnectTimeout time.Duration // 0 means no limit
 	HandleTimeout  time.Duration
 }
 
-// 默认选项
 var DefaultOption = &Option{
 	MagicNumber:    MagicNumber,
 	CodcType:       codec.GobType,
-	ConnectTimeout: time.Second * 10, //默认超时时长为10s
+	ConnectTimeout: time.Second * 10,
 }
 
-// rpc server
+// Server represents an RPC Server.
 type Server struct {
 	serviceMap sync.Map
 }
 
-// return a new Server
+// NewServer returns a new Server.
 func NewServer() *Server {
 	return &Server{}
 }
 
-// 默认server实例
+// DefaultServer is the default instance of *Server.
 var DefaultServer = NewServer()
 
-// 在监听器上接受连接并为请求提供服务
-func (server *Server) Accept(lis net.Listener) {
-	for {
-		conn, err := lis.Accept()
-		if err != nil {
-			log.Println("rpc server: accept error", err)
-			return
-		}
-		go server.ServerCon(conn)
-	}
-
-}
-
-func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
-
-/*
-| Option{MagicNumber: xxx, CodecType: xxx} | Header{ServiceMethod ...} | Body interface{} |
-| <------      固定 JSON 编码      ------>  | <-------   编码方式由 CodeType 决定   ------->|
-*/
-
-//最前面是JSON编码Option,后面Header和Body的编码格式由option的中的CodecType决定
-
-func (srever *Server) ServerCon(conn io.ReadWriteCloser) {
+// ServeConn runs the server on a single connection.
+// ServeConn blocks, serving the connection until the client hangs up.
+func (server *Server) ServeConn(conn io.ReadWriteCloser) {
 	defer func() { _ = conn.Close() }()
 	var opt Option
-
 	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
-		log.Println("rpc server: option error:", err)
+		log.Println("rpc server: options error: ", err)
 		return
 	}
-
 	if opt.MagicNumber != MagicNumber {
-		log.Printf("rpc server:invalid magic number %x", opt.MagicNumber)
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
 		return
 	}
-
-	//根据编码累类型获取相应函数
 	f := codec.NewCodecFuncMap[opt.CodcType]
 	if f == nil {
-		log.Printf("rpc server:invaild codec type %s", opt.CodcType)
+		log.Printf("rpc server: invalid codec type %s", opt.CodcType)
 		return
 	}
-
-	srever.serverCodec(f(conn), &opt)
-
+	server.serveCodec(f(conn), &opt)
 }
 
-// 无效请求，在发生错误响应时起占位作用
+// invalidRequest is a placeholder for response argv when error occurs
 var invalidRequest = struct{}{}
 
-/*sreverCodec
-  1.读取请求readRequest
-  2.处理请求handleRequest
-  3.回复请求sendRequest
-*/
-
-func (server *Server) serverCodec(cc codec.Codec, opt *Option) {
-	sending := new(sync.Mutex) //回复响应时并行回复
-	wg := new(sync.WaitGroup)
+func (server *Server) serveCodec(cc codec.Codec, opt *Option) {
+	sending := new(sync.Mutex) // make sure to send a complete response
+	wg := new(sync.WaitGroup)  // wait until all request are handled
 	for {
-		//读取请求
 		req, err := server.readRequest(cc)
 		if err != nil {
 			if req == nil {
-				break //无法恢复，关闭连接
+				break // it's not possible to recover, so close the connection
 			}
 			req.h.Error = err.Error()
-			//回复请求
 			server.sendResponse(cc, req.h, invalidRequest, sending)
 			continue
 		}
 		wg.Add(1)
-		//处理请求
 		go server.handleRequest(cc, req, sending, wg, opt.HandleTimeout)
 	}
-
-	wg.Wait() //等待所有request处理完成才close
+	wg.Wait()
 	_ = cc.Close()
 }
 
-// 存放一次请求的所有信息
+// request stores all information of a call
 type request struct {
-	h            *codec.Header
-	argv, replyv reflect.Value
+	h            *codec.Header // header of request
+	argv, replyv reflect.Value // argv and replyv of request
 	mtype        *methodType
 	svc          *service
 }
@@ -135,11 +103,31 @@ func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
 	var h codec.Header
 	if err := cc.ReadHeader(&h); err != nil {
 		if err != io.EOF && err != io.ErrUnexpectedEOF {
-			log.Println("rpc serve:read header error:", err)
+			log.Println("rpc server: read header error:", err)
 		}
 		return nil, err
 	}
 	return &h, nil
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server: service/method request ill-formed: " + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server: can't find service " + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc server: can't find method " + methodName)
+	}
+	return
 }
 
 func (server *Server) readRequest(cc codec.Codec) (*request, error) {
@@ -148,7 +136,6 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 		return nil, err
 	}
 	req := &request{h: h}
-
 	req.svc, req.mtype, err = server.findService(h.ServiceMethod)
 	if err != nil {
 		return req, err
@@ -156,29 +143,23 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 	req.argv = req.mtype.newArgv()
 	req.replyv = req.mtype.newReplyv()
 
-	//保证argvi是一个pointer,readbody需要一个pointer作为参数
-	//参数可能是一个pointer也可能不是pointer
+	// make sure that argvi is a pointer, ReadBody need a pointer as parameter
 	argvi := req.argv.Interface()
 	if req.argv.Type().Kind() != reflect.Ptr {
 		argvi = req.argv.Addr().Interface()
 	}
-
-	// //通过反射机制创建实际类型
-	// req.argv = reflect.New(reflect.TypeOf(""))
-
 	if err = cc.ReadBody(argvi); err != nil {
-		log.Println("rpc server: read argv err:", err)
-		return req, nil
+		log.Println("rpc server: read body err:", err)
+		return req, err
 	}
 	return req, nil
 }
 
 func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
-	//因为客户端只是一个，为了防止客户端数据接收混乱，所以需要加锁
 	sending.Lock()
 	defer sending.Unlock()
 	if err := cc.Write(h, body); err != nil {
-		log.Println("rpc server:write response error:", err)
+		log.Println("rpc server: write response error:", err)
 	}
 }
 
@@ -207,6 +188,7 @@ func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.
 	if timeout == 0 {
 		<-called
 		<-sent
+		return
 	}
 
 	select {
@@ -220,37 +202,75 @@ func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.
 	}
 }
 
-// 注册服务
+// Accept accepts connections on the listener and serves requests
+// for each incoming connection.
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+
+// Accept accepts connections on the listener and serves requests
+// for each incoming connection.
+func Accept(lis net.Listener) { DefaultServer.Accept(lis) }
+
+// Register publishes in the server the set of methods of the
+// receiver value that satisfy the following conditions:
+//   - exported method of exported type
+//   - two arguments, both of exported type
+//   - the second argument is a pointer
+//   - one return value, of type error
 func (server *Server) Register(rcvr interface{}) error {
 	s := newService(rcvr)
-
-	//注册到map当中
 	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
-		return errors.New("rpc:service already defined" + s.name)
+		return errors.New("rpc: service already defined: " + s.name)
 	}
 	return nil
 }
 
+// Register publishes the receiver's methods in the DefaultServer.
 func Register(rcvr interface{}) error { return DefaultServer.Register(rcvr) }
 
-// findService 通过serviceMethod从serviceMap中找到对应的service
-func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
-	dot := strings.LastIndex(serviceMethod, ".")
-	if dot < 0 {
-		err = errors.New("rpc server: service/method request ill-formed: " + serviceMethod)
+// 支持http
+const (
+	connected        = "200 Connected to Gee RPC"
+	defaultRPCPath   = "/_geerpc_"
+	defaultDebugpath = "/debug/geerpc"
+)
+
+func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	//不是CONNEXT
+	if req.Method != "CONNECT" {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, _ = io.WriteString(w, "405 must CONNECT\n")
 		return
 	}
-	//获取服务名和方法名
-	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
-	svci, ok := server.serviceMap.Load(serviceName)
-	if !ok {
-		err = errors.New("rpc server:can't find service " + serviceName)
+	//转换为Hijacker然后调用Hijack()可以返回TCP连接，此时连接状态将右调用者控制关闭
+	conn, _, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		log.Print("rpc hijacking", req.RemoteAddr, ": ", err.Error())
 		return
 	}
-	svc = svci.(*service)
-	mtype = svc.method[methodName]
-	if mtype == nil {
-		err = errors.New("rpc server:can't find method" + methodName)
-	}
-	return
+	_, _ = io.WriteString(conn, "HTTP/1.0"+connected+"\n\n")
+	//转换为TCP连接
+	server.ServeConn(conn)
 }
+
+
+//注册一个http处理程序
+func (server*Server)HandleHTTP(){
+	http.Handle(defaultRPCPath,server)
+}
+
+//默认http处理程序
+func HandleHTTP() {
+	DefaultServer.HandleHTTP()
+}
+
+
