862c9b3a55561230afd87a8ce8a239468deef040
gueFDF <3237455241@qq.com>
2023-01-14 15:11:38 +0800

    外部交互以及测试

---
+++ geecache/geecatche.go
@@ -0,0 +1,88 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// 函数类型实现某一个接口，称之为接口型函数，
+// 方便使用者在调用时既能够传入函数作为参数，
+// 也能够传入实现了该接口的结构体作为参数。
+// A Getter loads data for a key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+// 回调函数
+type GetterFunc func(key string) ([]byte, error)
+
+// 调用回调函数
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name      string //该缓存的名字
+	getter    Getter //缓存未命中时，获取资源的回调
+	mainCache cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	g := groups[name]
+	mu.RUnlock()
+	return g
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[GeeCache]hit")
+		return v, nil
+	}
+	//缓存未命中
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key)
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
