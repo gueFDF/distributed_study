35073f8a7bc70df343d32d9afaf1417a7b210600
gueFDF <3237455241@qq.com>
2023-01-17 21:33:44 +0800

    使用了protobuf进行通信

--- geecache/http.go
+++ geecache/http.go
@@ -3,12 +3,15 @@ package geecache
 import (
 	"fmt"
 	"geecache/consistenthash"
+	pb "geecache/geecachepb"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"sync"
+
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -42,6 +45,7 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
+	//"http://localhost:9999/_geecache/soures/Tom"
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
@@ -62,8 +66,15 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//利用protbuf将value转换为二进制编码发送给请求方
+	body, err := proto.Marshal(&pb.Response{Value: view.Byteslice()})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Count-Type", "application/octet-stream")
-	w.Write(view.Byteslice())
+	w.Write(body)
 }
 
 // 客户端
@@ -72,40 +83,43 @@ type httpGetter struct {
 }
 
 // 获取value
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
-	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(group), url.QueryEscape(key))
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
+	u := fmt.Sprintf("%v%v/%v", h.baseURL, url.QueryEscape(in.GetGroup()), url.QueryEscape(in.GetKey()))
+	//"http://localhost:9999/_geecache/soures/Tom"
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned: %v", res.Status)
+		return fmt.Errorf("server returned: %v", res.Status)
 	}
 	bytes, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("reading response: %v", err)
+		return fmt.Errorf("reading response: %v", err)
 	}
-	return bytes, nil
-
+	//解码
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body: %v", err)
+	}
+	return nil
 }
 
-
-//实例化一致性哈希算法，并添加了节点
+// 实例化一致性哈希算法，并添加了节点
 func (p *HTTPPool) Set(peers ...string) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 	p.peers = consistenthash.New(defaultReplicas, nil)
-	p.peers.Add(peers...)
+	p.peers.Add(peers...) //添加节点
 	p.httpGetters = make(map[string]*httpGetter, len(peers))
 	for _, peer := range peers {
 		p.httpGetters[peer] = &httpGetter{
 			baseURL: peer + p.basePath,
-		}
+		} //baseURL="http://localhost:9999" + "/_geecache/"
 	}
 }
 
-//包装一致性哈希的Get方法，获取服务器节点
+// 包装一致性哈希的Get方法，获取服务器节点
 func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
--- geecache/peers.go
+++ geecache/peers.go
@@ -1,4 +1,5 @@
 package geecache
+import pb "geecache/geecachepb"
 
 // 抽象接口，根据PickPeer()方法，根据传入key选择对应节点PerrGetter
 type PerrPicker interface {
@@ -7,5 +8,5 @@ type PerrPicker interface {
 
 // Get()方法用于从对应group查找缓存值
 type PeerGetter interface {
-	Get(group string, key string) ([]byte, error)
+	Get(in *pb.Request, out *pb.Response) error
 }
