487c750856504c90faa9b8254d72bc5a5502e4b1
gueFDF <3237455241@qq.com>
2023-03-21 21:41:39 +0800

    base2

--- gee/main.go
+++ gee/main.go
@@ -6,19 +6,23 @@ import (
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
-}
+// Engine is the uni handler for all requests
+type Engine struct{}
 
-func helloHandler(w http.ResponseWriter, req *http.Request) {
-	for k, v := range req.Header {
-		fmt.Fprintf(w, "Headler[%q]=%q\n", k, v)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	case "/hello":
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
 	}
 }
 
-
 func main() {
-	http.HandleFunc("/",indexHandler)
-	http.HandleFunc("/hello",helloHandler)
-	log.Fatal(http.ListenAndServe(":9999",nil))
+	engine := new(Engine)
+	log.Fatal(http.ListenAndServe(":9999", engine))
 }
